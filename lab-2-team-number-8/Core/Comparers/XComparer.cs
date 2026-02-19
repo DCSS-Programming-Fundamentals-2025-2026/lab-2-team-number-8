b7fd45475a7ca79fefd6a08b1722136c73969ecf
marineKP52 <gls.maryna@gmail.com>
2026-02-19 23:34:47 +0200

    Upgraded.

---
+++ lab-2-team-number-8/Core/Comparers/XComparer.cs
@@ -0,0 +1,21 @@
+﻿using lab_2_team_number_8;
+using System.Collections;
+
+public class XComparer : IComparer
+{
+    public int Compare(object x, object y)
+    {
+        Delivery d1 = (Delivery)x;
+        Delivery d2 = (Delivery)y;
+
+        int priorityResult = d2.Priority.CompareTo(d1.Priority);
+
+        if (priorityResult != 0)
+        {
+            return priorityResult;
+        }
+
+        return d1.Id.CompareTo(d2.Id);
+    }
+}
+
---
+++ lab-2-team-number-8/Core/DeliveryEnumerator.cs
@@ -0,0 +1,36 @@
+﻿using lab_2_team_number_8;
+using System.Collections;
+
+public class DeliveryEnumerator : IEnumerator
+{
+    private Delivery[] _deliveries;
+    private int _position = -1;
+
+    public DeliveryEnumerator(Delivery[] deliveries)
+    {
+        _deliveries = deliveries;
+    }
+
+    public bool MoveNext()
+    {
+        _position++;
+        
+        if (_position < _deliveries.Length)
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
+    public object Current
+    {
+        get { return _deliveries[_position]; }
+    }
+
+    public void Reset()
+    {
+        _position = -1;
+    }
+}
+
